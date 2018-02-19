c93592196ca9198f7dcba23158f0b88b807d2244
Will Palmer <will@do-it-well.co.uk>
2018-02-19 22:18:50 +0000

    teach ECS partial about overlay2 driver
    ECS Optimised instances default to using the "devicemapper" storage
    driver for docker. This default is useful in a large cluster in which
    the images are not known in advance, but is extremely inefficient in
    situations where most containers are known to share the same image.
    
    Using the overlay2 driver allows these "shared" images to be shared in
    memory as well, vastly increasing memory efficiency and vastly reducing
    disk IO requirements. In theory. This change is being written based on
    the docker storage driver documentation, and has not yet been tested in
    a "real-world" situation.

--- partials/CloudFormationInit/ConfigSet/ECS.rb
+++ partials/CloudFormationInit/ConfigSet/ECS.rb
@@ -1,5 +1,6 @@
 Proc.new do |
-	clusterName: nil # name of the ECS Cluster to attach to
+	clusterName: nil, # name of the ECS Cluster to attach to
+	storageDriver: nil # the docker storage driver to use
 |
 	{
 		"commands" => {
@@ -41,6 +42,66 @@ Proc.new do |
 				END_SH
 				)
 			},
+			"20_configure_storage_driver" => {
+				"test" =>
+					if storageDriver.nil?
+						"#!/bin/bash -xe\n# no storageDriver specified\nexit 1\n"
+					else
+						interpolate(<<-END_SH.gsub(/^\s+/, ""), {driver: storageDriver}
+							#!/bin/bash -xe
+							cfg='DOCKER_STORAGE_OPTIONS="--storage-driver {{locals[:driver]}}"'
+							if [[ -e /etc/sysconfig/docker-storage ]] && grep -q -F "$cfg" /etc/sysconfig/docker-storage; then
+								false
+							else
+								true
+							fi
+							END_SH
+						)
+					end,
+				"command" =>
+					if storageDriver.nil?
+						"#!/bin/bash -xe\n# no storageDriver specified\nexit 1\n"
+					elsif storageDriver == "overlay2"
+						interpolate(<<-END_SH.gsub(/^\s+/, "")
+							#!/bin/bash -xe
+							pool=/dev/docker/docker-pool
+							target=/var/lib/docker/overlay2
+
+							## Stop Docker before we mess with it
+							service docker stop
+
+							## Ensure Device is Mounted
+							mkdir -p ${target}
+							grep -q ${target} /etc/fstab ||
+							printf '%s %s ext4 defaults 0 0\\n' \\
+								"${pool}" \\
+								"${target}" \\
+								>> /etc/fstab
+
+							if ! grep -q ${target} /etc/mtab; then
+								if ! mount ${pool} 2>&-; then
+									# Mounting failed, assume this is due to wrong format
+									mkfs -t ext4 ${pool}
+									mount ${pool}
+								fi
+							fi
+
+							## Use overlay2 for Storage
+							cfg='DOCKER_STORAGE_OPTIONS="--storage-driver overlay2"'
+							echo "$cfg" > /etc/sysconfig/docker-storage
+
+							service docker start
+							END_SH
+						)
+					else
+						interpolate(<<-END_SH.gsub(/^\s+/, ""), {driver: storageDriver}
+							#!/bin/bash -xe
+							cfg='DOCKER_STORAGE_OPTIONS="--storage-driver {{locals[:driver]}}"'
+							echo "$cfg" > /etc/sysconfig/docker-storage
+							END_SH
+						)
+					end
+			},
 			"50_add_instance_to_cluster" => {
 				"test" => interpolate(<<-END_SH.gsub(/^\s+/, ""), {cn: clusterName}
 					#!/bin/bash -xe,
@@ -60,6 +121,15 @@ Proc.new do |
 				END_SH
 				)
 			}
+		},
+		"packages" => {
+			"yum" => (
+				if storageDriver == "overlay2"
+					{"parted" => []}
+				else
+					{ }
+				end
+			)
 		}
 	}
 end
